11f8d9a025d1f60cdb70c4243d18d6f246009438
Parapets <southwest85b@gmail.com>
2020-10-24 19:06:35 +0100

    Settings architecture

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -1,35 +1,11 @@
 #include "Hooks.h"
-#include "Offsets.h"
+#include "ArrowWeightManager.h"
 
 namespace Hooks
 {
-	// TODO: move to own file
-	class SurvivalHookManager
-	{
-	public:
-		inline static void Install()
-		{
-			REL::Relocation<std::uintptr_t> GetWeight_ArrowWeight_Hook{ TESBoundObject_GetWeight_offset, 0x7C };
-
-			auto& trampoline = SKSE::GetTrampoline();
-			_IsArrowWeightEnabled = trampoline.write_call<5>(GetWeight_ArrowWeight_Hook.address(), IsArrowWeightEnabled);
-
-			logger::info("Installed hook for arrow weight"sv);
-		}
-
-	private:
-		inline static bool IsArrowWeightEnabled()
-		{
-			return true;
-			//return _IsArrowWeightEnabled();
-		}
-
-		inline static REL::Relocation<decltype(IsArrowWeightEnabled)> _IsArrowWeightEnabled;
-	};
-
 	void Install()
 	{
-		SurvivalHookManager::Install();
+		ArrowWeightManager::Install();
 
 		logger::info("Installed all hooks"sv);
 	}
---
+++ src/Survival.cpp
@@ -0,0 +1,61 @@
+#include "Survival.h"
+
+namespace Survival
+{
+	void Settings::ForceEnable()
+	{
+		ModPreference = Preference::AlwaysEnabled;
+	}
+
+	void Settings::RequestDisable()
+	{
+		if (ModPreference == Preference::None)
+		{
+			ModPreference = Preference::AlwaysDisabled;
+		}
+	}
+
+	void Settings::ModReset()
+	{
+		ModPreference = Preference::None;
+	}
+
+	void Settings::UserEnable()
+	{
+		UserPreference = Preference::AlwaysEnabled;
+	}
+
+	void Settings::UserDisable()
+	{
+		UserPreference = Preference::AlwaysDisabled;
+	}
+
+	void Settings::UserReset()
+	{
+		UserPreference = Preference::None;
+	}
+
+	bool Settings::IsEnabled(SurvivalModeCallback gameCallback)
+	{
+		if (UserPreference == Preference::AlwaysEnabled)
+		{
+			return true;
+		}
+		else if (UserPreference == Preference::AlwaysDisabled)
+		{
+			return false;
+		}
+		else if (ModPreference == Preference::AlwaysEnabled)
+		{
+			return true;
+		}
+		else if (ModPreference == Preference::AlwaysDisabled)
+		{
+			return false;
+		}
+		else
+		{
+			return gameCallback();
+		}
+	}
+}
