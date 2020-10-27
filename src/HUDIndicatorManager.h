fcfd280c314f01602b8155ce17874ad323de0cee
Colin Kelley <colinkelley30@gmail.com>
2020-10-26 21:20:53 -0500

    Final code changes for push

--- src/HUDIndicatorManager.h
+++ src/HUDIndicatorManager.h
@@ -6,14 +6,17 @@
 class HUDIndicatorManager
 {
 public:
+	//TODO DOES NOT WORK NEED TO FIX
 	inline static void Install()
 	{
+		/*
 		REL::Relocation<std::uintptr_t> HUDWidget_Hook{ TweenMenu_Sub_offset, 0x7B };
 
 		auto& trampoline = SKSE::GetTrampoline();
 		trampoline.write_call<5>(HUDWidget_Hook.address(), IsHUDIndicatorEnabled);
 
 		logger::info("Installed hook for HUD Indicator."sv);
+		*/
 	}
 
 private:
